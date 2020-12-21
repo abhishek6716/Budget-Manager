96a8824c76292d2cb4a0dca9cf9b00835224ed88
Abhishek singh <abhisheksingh6716@gmail.com>
2020-12-21 23:09:12 +0530

    update js file

--- app.js
+++ app.js
@@ -147,7 +147,8 @@ var UIController=(function(){
         expansesLabel: '.budget__expenses--value',
         percentageLabel: '.budget__expenses--percentage',
         container: '.container',
-        expensesPercLabel: '.item__percentage'
+        expensesPercLabel: '.item__percentage',
+        dateLabel: '.budget__title--month'
     };
 
     return{
@@ -231,6 +232,17 @@ var UIController=(function(){
                     current.textContent = '---';
                 }
             });
+        },
+
+        displayDate: function(){
+            var now, month, year, months;
+
+            now = new Date();
+            months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+            month = now.getMonth();
+            year = now.getFullYear();
+
+            document.querySelector(DOMstrings.dateLabel).textContent = months[month] + ' ' + year;
         },
 
         getDOMstrings: function(){
@@ -339,6 +351,7 @@ var controller=(function(budgetCtrl, UIctrl){
     return{
         init: function(){
             console.log('application has started');
+            UIctrl.displayDate(); 
             UIctrl.displayBudget({
                 budget: 0,
                 totalInc: 0,
